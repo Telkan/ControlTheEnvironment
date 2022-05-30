67d969e74866908fa636bbc5b1bb72f5ef3981eb
Telkan <gamba.gael@orange.fr>
2022-05-30 18:50:21 +0200

    Teste teste

--- TestZone/TestDisplay.gd
+++ TestZone/TestDisplay.gd
@@ -1,15 +1,11 @@
-extends RigidBody2D
+extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
+var velocity = Vector2(5,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	linear_velocity = Vector2(150,0)
 	pass # Replace with function body.
 
 func calculate_gravity(satellite_pos, planet_pos, satellite_mass, planet_mass):
@@ -17,9 +13,9 @@ func calculate_gravity(satellite_pos, planet_pos, satellite_mass, planet_mass):
 	var gravForce = (SpacePhysics.gravity_constant * satellite_mass * planet_mass) / (satellite_pos.distance_to(planet_pos) *  (satellite_pos.distance_to(planet_pos))) 
 	var gravity = gravDir * gravForce
 	
-	applied_force = gravity
+	velocity += gravity
 
 
 func _physics_process(delta):
-	calculate_gravity(get_position(), get_parent().find_node("Sun").get_position(), 50, 5000)
-	#set_position(get_position()+velocity)
+	calculate_gravity(get_position(), get_parent().find_node("Sun").get_position(), 50, 500)
+	set_position(get_position()+velocity)
