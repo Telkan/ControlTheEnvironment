c0d6ed5702fa77f3602466af4fad4a23f6eac647
Telkan <gamba.gael@orange.fr>
2022-06-05 13:50:33 +0200

    POST PROCESSING POG

--- Elements/Sun/Sun.gd
+++ Elements/Sun/Sun.gd
@@ -6,7 +6,7 @@ extends StaticBody2D
 # var b = "text"
 var velocity = Vector2(0,0)
 var applied_force = Vector2(0,0)
-export var planetMass := 10 
+export var planetMass := 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
